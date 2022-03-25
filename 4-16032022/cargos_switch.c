b19f935da1464748e0441e23cdcab042a6b1aef4
ViRosa <vitoriadanielrosa@gmail.com>
2022-03-25 09:18:00 -0300

    Criando o arquiva README

---
+++ 4-16032022/cargos_switch.c
@@ -0,0 +1,22 @@
+/*Programa para exibir o salário do
+cargo de acordo com a escolha do 
+usuário
+*/
+#include <stdio.h>
+
+int main(){
+
+    int opcao = 0
+
+    //comando para limpar a tela
+    clrscr();//clear screen(limpar a tela)
+    printf("#######################################################");
+    printf("\n#\t Programa para exibir salário\t\t\t#\n");
+    printf("########################################################\n");
+    printf("\nEscolha um dos cargos abaixo digitando o número correspondente");
+    printf("\t1 - Diretor\n\t2 - Gerente\n\t3 - Analista\n\t4 - Assistente\n\t5 - ")
+
+    
+    
+    return 0;
+}
